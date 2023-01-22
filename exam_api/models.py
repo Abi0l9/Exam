b7b079ad599c2e62875d3d715dc996382488492a
Abi0l9 <cryptog3ms@gmail.com>
2023-01-22 20:47:43 +0100

    made some additions and changes to models

--- exam_api/models.py
+++ exam_api/models.py
@@ -5,69 +5,79 @@ class Exam(models.Model):
   name = models.CharField(max_length = 255)
   
   class Meta:
-		verbose_name_plural = "Exams"
-		ordering = ['id']
+    verbose_name_plural = "Exams"
+    ordering = ['id']
   
   def __str__(self):
-		return self.name
+    return self.name
 
   
 class Year(models.Model):
   year = models.IntegerField()
-  title = models.ForeignKey(Exam, on_delete=models.DO_NOTHING,
-  related_name='exam_name')
+  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING)
   
   class Meta:
-		verbose_name_plural = "Years"
-		ordering = ['id']
+    verbose_name_plural = "Years"
+    ordering = ['id']
   
   def __str__(self):
-		return self.year
+    return self.year
 
   
 class Month(models.Model):
   name = models.CharField(max_length = 255)
-  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING,
-  related_name='exam_year')
-  title = models.ForeignKey(Exam, on_delete=models.DO_NOTHING,
-  related_name='exam_name')
+  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING)
+  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING)
   
   class Meta:
-		verbose_name_plural = "Months"
-		ordering = ['id']
+    verbose_name_plural = "Months"
+    ordering = ['id']
 
   def __str__(self):
-		return self.name
+    return self.name
+		
+class Subject(models.Model):
+  name = models.CharField(max_length = 255)
+  month = models.ForeignKey(Month, on_delete=models.DO_NOTHING)
+  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING)
+  exam_name = models.ForeignKey(Exam, on_delete=models.DO_NOTHING)
+  
+  class Meta:
+    verbose_name_plural = "Subjects"
+    ordering = ['id']
+
+  def __str__(self):
+    return self.name
 
   
 class Question(models.Model):
   question_text = models.TextField()
-  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING,
-  related_name='exam_year')
-  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING,
-  related_name='exam_name')
+  subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING)
+  month = models.ForeignKey(Month, on_delete=models.DO_NOTHING)
+  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING)
   
   class Meta:
-		verbose_name_plural = "Questions"
-		ordering = ['id']
+    verbose_name_plural = "Questions"
+    ordering = ['id']
   
   def __str__(self):
-		return self.question_text
+    return self.question_text
 
   
 class Answer(models.Model):
-  answer = models.charField(max_length=255)
+  answer = models.CharField(max_length=255)
   correct = models.BooleanField(default=False)
-  question = models.ForeignKey(Question, on_delete=models.DO_NOTHING,
-  related_name='question')
-  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING,
-  related_name='exam_year')
-  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING,
-  related_name='exam_name')
+  question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+  subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+  month = models.ForeignKey(Month, on_delete=models.DO_NOTHING)
+  month = models.ForeignKey(Month, on_delete=models.DO_NOTHING)
+  year = models.ForeignKey(Year, on_delete=models.DO_NOTHING)
+  exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING)
   
   class Meta:
-		verbose_name_plural = "Answers"
-		ordering = ['id']
+    verbose_name_plural = "Answers"
+    ordering = ['id']
 
   def __str__(self):
-		return self.answer
+    return self.answer
